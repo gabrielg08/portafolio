8163aef15bba319ce52784302c595c00098c9e2b
gabrielg08 <68970721+gabrielg08@users.noreply.github.com>
2023-11-01 16:22:07 -0400

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,7 @@ export default function Tech() {
     return (
       <div id='tech' className="bg-violet-700 py-24 sm:py-32 max-w-4xl mx-auto p-4 rounded-lg">
         <div className="max-w-4xl mx-auto p-4">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-400">
+          <h2 className="text-center text-2xl font-semibold leading-8 text-white">
           Technology I use..
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
